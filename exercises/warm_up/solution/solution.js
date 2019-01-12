62d52e74d31196e83cf0875e3258c0a36074267e
Manil Chowdhury <manil.chowdhury@gmail.com>
2019-01-12 14:32:57 -0800

    Update english solution

--- exercises/warm_up/solution/solution.js
+++ exercises/warm_up/solution/solution.js
@@ -1,9 +1,3 @@
-'use strict';
+// After 300ms has elapsed, print out 'TIMED OUT!'
 
-// Set a timeout of 300ms
-
-setTimeout(function () {
-  // After 300ms has elapsed, print out 'TIMED OUT!'
-
-  console.log('TIMED OUT!');
-}, 300);
+setTimeout(console.log, 300, 'TIMED OUT!')
